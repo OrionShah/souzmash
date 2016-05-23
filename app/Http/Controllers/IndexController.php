5c49039f6d616e33130b9d0fb77579abfea9e4cc
Andrey Shahov <orionshah@yandex.ru>
2016-05-23 20:19:56 +0300

    Правки стилей и контроллера

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -66,36 +66,6 @@ class IndexController extends Controller
         return redirect('/');
     }
 
-    public function getPost($id)
-    {
-        $post = news::find($id);
-        if (!$post) {
-            return redirect('/');
-        }
-
-        if (!$post->is_publish) {
-            return redirect('/');
-        }
-
-        $options = [
-            'post' => $post,
-            'menus' => $this->menus(),
-        ];
-        if ($post->comments) {
-            $comments = Comments::where('post', '=', $post->id);
-            $comments_array = $comments->get();
-            foreach ($comments_array as $key => $comment) {
-                $comment->time = $comment->created_at->format("d.m.Y H:i");
-                $comments_array[$key] = $comment;
-            }
-
-            $options['comments_count'] = $comments->count();
-            $options['comments'] = $comments_array;
-        }
-
-        return view('post', $options);
-    }
-
     public function postLogout()
     {
         Auth::logout();
